cb5c94f929bba1c8d0fd04358be9198d54d04a0c
夕舞八弦 <1677759063@qq.com>
2021-10-31 16:33:35 +0800

    feat: windicss eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { render } from "preact";
-
+import "virtual:windi.css";
 import { ItemCharReq } from "./item-char-req";
 if (location.href.includes("Widget:Item-character-requirement")) {
   render(<ItemCharReq />, document.getElementById("root")!);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from "windicss/helpers";
 
-export default defineConfig({});
+export default defineConfig({
+  theme: {
+    colors: {
+      divider: "#a2a9b1",
+      primary: "#22bbff",
+      disabled: "#9d9d9d",
+    },
+  },
+});
