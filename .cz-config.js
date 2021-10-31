db168faa4e36f7341495ab7108af696d5bda93b5
夕舞八弦 <1677759063@qq.com>
2021-10-31 12:16:53 +0800

    feat: windicss md5

--- .cz-config.js
+++ .cz-config.js
@@ -1,34 +1,34 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:\t新功能' },
-    { value: 'fix', name: 'fix:\t修复' },
-    { value: 'docs', name: 'docs:\t文档变更' },
-    { value: 'style', name: 'style:\t代码格式(不影响代码运行的变动)' },
+    { value: "feat", name: "feat:\t新功能" },
+    { value: "fix", name: "fix:\t修复" },
+    { value: "docs", name: "docs:\t文档变更" },
+    { value: "style", name: "style:\t代码格式(不影响代码运行的变动)" },
     {
-      value: 'refactor',
-      name: 'refactor: 重构(既不是增加feature，也不是修复bug)'
+      value: "refactor",
+      name: "refactor: 重构(既不是增加feature，也不是修复bug)",
     },
-    { value: 'perf', name: 'perf:\t性能优化' },
-    { value: 'test', name: 'test:\t增加测试' },
-    { value: 'chore', name: 'chore:\t构建过程或辅助工具的变动' },
-    { value: 'revert', name: 'revert:\t回退' },
-    { value: 'build', name: 'build:\t打包' }
+    { value: "perf", name: "perf:\t性能优化" },
+    { value: "test", name: "test:\t增加测试" },
+    { value: "chore", name: "chore:\t构建过程或辅助工具的变动" },
+    { value: "revert", name: "revert:\t回退" },
+    { value: "build", name: "build:\t打包" },
   ],
   // override the messages, defaults are as follows
   messages: {
-    type: '请选择提交类型:',
+    type: "请选择提交类型:",
     // scope: '请输入文件修改范围(可选):',
     // used if allowCustomScopes is true
-    customScope: '请输入修改范围(可选):',
-    subject: '请简要描述提交(必填):',
-    body: '请输入详细描述(可选，待优化去除，跳过即可):',
+    customScope: "请输入修改范围(可选):",
+    subject: "请简要描述提交(必填):",
+    body: "请输入详细描述(可选，待优化去除，跳过即可):",
     // breaking: 'List any BREAKING CHANGES (optional):\n',
-    footer: '请输入要关闭的issue(待优化去除，跳过即可):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
+    footer: "请输入要关闭的issue(待优化去除，跳过即可):",
+    confirmCommit: "确认使用以上信息提交？(y/n/e/h)",
   },
   allowCustomScopes: true,
   // allowBreakingChanges: ['feat', 'fix'],
-  skipQuestions: ['body', 'footer'],
+  skipQuestions: ["body", "footer"],
   // limit subject length, commitlint默认是72
-  subjectLimit: 72
-}
+  subjectLimit: 72,
+};
