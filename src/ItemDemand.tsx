a625a9fbc94889b1e4208c50a4ce7b685c347037
夕舞八弦 <1677759063@qq.com>
2021-11-18 22:00:23 +0800

    feat: 模组

--- src/ItemDemand.tsx
+++ src/ItemDemand.tsx
@@ -10,8 +10,17 @@ interface costProps {
   elite: number; //精英化
   skill: number; //技能1-7
   mastery: [number, number, number]; //技能专精
+  uniequip: number; //模组
 }
-function Cost({ rarity, name, profession, elite, skill, mastery }: costProps) {
+function Cost({
+  rarity,
+  name,
+  profession,
+  elite,
+  skill,
+  mastery,
+  uniequip,
+}: costProps) {
   return (
     <div class="flex flex-col justify-center items-center my-8px ">
       <Avatar rarity={rarity} name={name} profession={profession} size={60} />
@@ -35,6 +44,12 @@ function Cost({ rarity, name, profession, elite, skill, mastery }: costProps) {
           {sum(mastery) === 0 ? 0 : `${mastery.join("/")}`}
         </span>
       </div>
+      <div class={`${uniequip === 0 ? "text-disabled" : ""}`}>
+        模组：
+        <span class={`${uniequip !== 0 ? "text-primary-main" : ""}  font-bold`}>
+          {uniequip}
+        </span>
+      </div>
     </div>
   );
 }
@@ -49,6 +64,7 @@ interface itemCost {
     skill: number;
     mastery: number;
     total: number;
+    uniequip: number;
   };
 }
 interface resp {
@@ -58,10 +74,11 @@ async function query(name: string): Promise<itemCost> {
   const { data } = await api.get(`/widget/itemDemand/${name}`);
   console.log(data);
   const costs = new Array<cost>(6);
-  let total = {
+  const total = {
     elite: 0,
     skill: 0,
     mastery: 0,
+    uniequip: 0,
     total: 0,
   };
   Object.keys(data as resp).forEach((key) => {
@@ -70,13 +87,15 @@ async function query(name: string): Promise<itemCost> {
     cost.data.push(v);
     costs[v.rarity] = cost;
     total.elite += v.elite;
+    total.uniequip += v.uniequip;
     if (key !== "char_1001_amiya2") {
       // 剑兔的技能1-7的需求在阿米娅那算过了
       total.skill += v.skill;
     }
     total.mastery += v.mastery.reduce((acc, cur) => acc + cur, 0);
   });
-  total.total = total.elite + total.skill + total.mastery;
+  total.total = total.elite + total.skill + total.mastery + total.uniequip;
+  console.log(total);
   return {
     costs: costs.filter((v) => v).reverse(), // 让六星排到前面
     total,
@@ -127,6 +146,7 @@ export function ItemDemand({ item }: props) {
               <div>精英化：{data.total.elite}</div>
               <div>技能1→7：{data.total.skill}</div>
               <div>技能专精：{data.total.mastery}</div>
+              <div>模组：{data.total.uniequip}</div>
               <div class="font-bold">总计：{data.total.total}</div>
             </div>
             <Tabs
@@ -136,22 +156,25 @@ export function ItemDemand({ item }: props) {
                 setSelected(i);
               }}
               selected={selected}
-            ></Tabs>
+            />
             <div class="mt-12px">
               {data.costs.map((cost, i) => (
                 <div
+                  key={i}
                   class={`grid grid-cols-5 <sm:grid-cols-3 ${
                     selected == i ? "" : "hidden"
                   }`}
                 >
                   {cost.data.map((v) => (
                     <Cost
+                      key={v.name}
                       rarity={v.rarity}
                       name={v.name}
                       profession={v.profession}
                       elite={v.elite}
                       skill={v.skill}
                       mastery={v.mastery}
+                      uniequip={v.uniequip}
                     />
                   ))}
                 </div>
@@ -160,6 +183,7 @@ export function ItemDemand({ item }: props) {
           </>
         ) : (
           <>
+            <Skeleton classes="w-120px h-30px my-8px" />
             <Skeleton classes="w-120px h-30px my-8px" />
             <Skeleton classes="w-120px h-30px my-8px" />
             <Skeleton classes="w-120px h-30px my-8px" />
